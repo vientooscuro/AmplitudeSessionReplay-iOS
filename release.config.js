6fa8de7289ebb2b2e7520e12f8422e79ff68c1cb
Chris Leonavicius <chris.leonavicius@amplitude.com>
2024-10-22 14:13:50 -0700

    chore: update build scripts

---
+++ release.config.js
@@ -0,0 +1,64 @@
+module.exports = {
+  "branches": ["main"],
+  "plugins": [
+    ["@semantic-release/commit-analyzer", {
+      "preset": "angular",
+      "parserOpts": {
+        "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+      }
+    }],
+    ["@semantic-release/release-notes-generator", {
+      "preset": "angular",
+    }],
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md"
+    }],
+    "@semantic-release/github",
+    [
+      "@google/semantic-release-replace-plugin",
+      {
+        "replacements": [
+          {
+            "files": ["AmplitudeSwift.podspec"],
+            "from": "amplitude_version = \".*\"",
+            "to": "amplitude_version = \"${nextRelease.version}\"",
+            "results": [
+              {
+                "file": "AmplitudeSessionReplay.podspec",
+                "hasChanged": true,
+                "numMatches": 1,
+                "numReplacements": 1
+              },
+              {
+                "file": "AmplitudeiOSSessionReplayMiddleware.podspec",
+                "hasChanged": true,
+                "numMatches": 1,
+                "numReplacements": 1
+              },
+              {
+                "file": "AmplitudeSwiftSessionReplayPlugin.podspec",
+                "hasChanged": true,
+                "numMatches": 1,
+                "numReplacements": 1
+              },
+            ],
+            "countMatches": true
+          },
+        ]
+      }
+    ],
+    ["@semantic-release/git", {
+      "assets": ["AmplitudeSessionReplay.podspec", "AmplitudeiOSSessionReplayMiddleware.podspec", "AmplitudeSwiftSessionReplayPlugin.podspec", "CHANGELOG.md"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    ["@semantic-release/exec", {
+      "publishCmd": "pod trunk push AmplitudeSessionReplay.podspec --allow-warnings",
+    }],
+    ["@semantic-release/exec", {
+      "publishCmd": "pod trunk push AmplitudeiOSSessionReplayMiddleware.podspec --allow-warnings",
+    }],
+    ["@semantic-release/exec", {
+      "publishCmd": "pod trunk push AmplitudeSwiftSessionReplayPlugin.podspec --allow-warnings",
+    }],
+  ],
+}
